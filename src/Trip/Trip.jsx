cd76ec7aa186ac772bbdc18075c9b432818f10ac
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-08-28 13:04:21 +0300

    mob fixes

--- src/Trip/Trip.jsx
+++ src/Trip/Trip.jsx
@@ -24,28 +24,30 @@ const Trip = () => {
                     Затем сошли с тропы и провели по 2 дня в каждом из городов: Бахчисарай, Севастополь и Симферополь.
                 </p>
             </div>
-            <div className={classes.day}>
-                <EachDayContainer day={1} addHideText={hideTripText[1]} />
-                <EachDayContainer day={2} addHideText={hideTripText[2]} />
-            </div>
-            <div className={classes.day}>
-                <EachDayContainer day={3} addHideText={hideTripText[3]} />
-                <EachDayContainer day={4} addHideText={hideTripText[4]} />
-            </div>
-            <div className={classes.day}>
-                <EachDayContainer day={5} desc={dayDescriptions[5]} addHideText={hideTripText[5]} />
-                <EachDayContainer day={6} desc={dayDescriptions[6]} addHideText={hideTripText[6]} />
-            </div>
-            <div className={classes.day}>
-                <EachDayContainer day={7} addHideText={hideTripText[7]} />
-                <EachDayContainer day={8} addHideText={hideTripText[8]} />
-            </div>
-            <div className={classes.day}>
-                <EachDayContainer day={9} addHideText={hideTripText[9]} />
-                <EachDayContainer day={10} addHideText={hideTripText[10]} />
-            </div>
-            <div className={classes.day}>
-                <EachDayContainer day={11} addHideText={hideTripText[11]} />
+            <div className={classes.allDays}>
+                <div className={classes.day}>
+                    <EachDayContainer day={1} addHideText={hideTripText[1]} />
+                    <EachDayContainer day={2} addHideText={hideTripText[2]} />
+                </div>
+                <div className={classes.day}>
+                    <EachDayContainer day={3} addHideText={hideTripText[3]} />
+                    <EachDayContainer day={4} addHideText={hideTripText[4]} />
+                </div>
+                <div className={classes.day}>
+                    <EachDayContainer day={5} desc={dayDescriptions[5]} addHideText={hideTripText[5]} />
+                    <EachDayContainer day={6} desc={dayDescriptions[6]} addHideText={hideTripText[6]} />
+                </div>
+                <div className={classes.day}>
+                    <EachDayContainer day={7} addHideText={hideTripText[7]} />
+                    <EachDayContainer day={8} addHideText={hideTripText[8]} />
+                </div>
+                <div className={classes.day}>
+                    <EachDayContainer day={9} addHideText={hideTripText[9]} />
+                    <EachDayContainer day={10} addHideText={hideTripText[10]} />
+                </div>
+                <div className={classes.day}>
+                    <EachDayContainer day={11} addHideText={hideTripText[11]} />
+                </div>
             </div>
             <p className={classes.footer}>© 2021 Мама и Дети, Крым</p>
         </div>
